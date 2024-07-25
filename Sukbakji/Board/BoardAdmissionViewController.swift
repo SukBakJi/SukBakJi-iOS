dda9b9bc571d52991c7e140fff3311020a8b6ce8
CTOKKM <gkzl95@naver.com>
2024-07-25 16:17:41 +0900

    [fix]충돌 사항 해결

---
+++ Sukbakji/Board/BoardAdmissionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BoardAdmissionViewController.swift
+//  Sukbakji
+//
+//  Created by KKM on 7/25/24.
+//
+
+import SwiftUI
+
+struct BoardAdmissionViewController: View {
+    var body: some View {
+        Text("입학 예정 화면")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+#Preview {
+    BoardAdmissionViewController()
+}
---
+++ Sukbakji/Board/BoardMasterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BoardMasterViewController.swift
+//  Sukbakji
+//
+//  Created by KKM on 7/25/24.
+//
+
+import SwiftUI
+
+struct BoardMasterViewController: View {
+    var body: some View {
+        Text("석사 화면")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+#Preview {
+    BoardMasterViewController()
+}
