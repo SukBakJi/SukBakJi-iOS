d3d07d93332968d8ac416891a886fcd6f28fb25b
Jaewift <pjk971113@naver.com>
2024-07-26 20:33:56 +0900

    [feat] 홈UI 수정 완료 및 캘린더 UI 작업중

--- Sukbakji/SceneDelegate.swift
+++ Sukbakji/SceneDelegate.swift
@@ -15,14 +15,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     // SiwftUI로 BoardViewController 실행하기
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        
-        let mainViewController = UINavigationController(rootViewController: LoginViewController())
-        //let mainViewController = UINavigationController(rootViewController: successSignUpViewController())
-        
-        window?.rootViewController = mainViewController
-        window?.makeKeyAndVisible()
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
     
 //    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
