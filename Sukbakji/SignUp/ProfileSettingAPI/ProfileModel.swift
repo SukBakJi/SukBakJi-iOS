b6f35ec17359ca375dc08d1d92364213d7f1234b
hyunm1n-o <hmoh0703@naver.com>
2024-08-12 00:54:08 +0900

    [feat] 프로필 설정 api 연동 완료

---
+++ Sukbakji/SignUp/ProfileSettingAPI/ProfileModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileModel.swift
+//  Sukbakji
+//
+//  Created by 오현민 on 8/12/24.
+//
+
+import Foundation
---
+++ Sukbakji/SignUp/ResearchTopic/ResearchTopicAPI/ResearchTopicDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  ResearchTopicDataManager.swift
+//  Sukbakji
+//
+//  Created by 오현민 on 8/11/24.
+//
+
+import Foundation
