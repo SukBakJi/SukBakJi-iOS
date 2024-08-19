66cf7d92a4caf33a76508c38fe2f88abd663c7ee
Jaewift <pjk971113@naver.com>
2024-08-20 01:08:00 +0900

    [feat] 홈화면 API 연결 및 UI 수정

--- Sukbakji/Home/MainHome/HotFeedViewController.swift
+++ Sukbakji/Home/MainHome/HotFeedViewController.swift
@@ -6,32 +6,66 @@
 //
 
 import UIKit
+import Alamofire
 
 class HotFeedViewController: UIViewController {
     
     @IBOutlet weak var HotFeedTV: UITableView!
+    
+    var allDatas: [HotPostResult] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         HotFeedTV.delegate = self
         HotFeedTV.dataSource = self
-        HotFeedTV.layer.masksToBounds = false// any value you want
+        HotFeedTV.layer.masksToBounds = true// any value you want
         HotFeedTV.layer.shadowOpacity = 0.2// any value you want
         HotFeedTV.layer.shadowRadius = 2 // any value you want
         HotFeedTV.layer.shadowOffset = .init(width: 0, height: 0.2)
         HotFeedTV.contentInset = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
+        
+        self.getHotBoard()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getHotBoard() {
+        guard let retrievedToken = KeychainHelper.standard.read(service: "access-token", account: "user", type: String.self) else {
+            print("Failed to retrieve password.")
+            return
+        }
+        
+        let url = APIConstants.communityURL + "/hot-boards"
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(retrievedToken)",
+        ]
+        
+        AF.request(url, method: .get, headers: headers).responseData { response in
+            switch response.result {
+            case .success(let data):
+                do {
+                    let decodedData = try JSONDecoder().decode(HotPostResultModel.self, from: data)
+                    self.allDatas = decodedData.result
+                    DispatchQueue.main.async {
+                        self.HotFeedTV.reloadData()
+                    }
+                } catch let DecodingError.dataCorrupted(context) {
+                    print(context)
+                } catch let DecodingError.keyNotFound(key, context) {
+                    print("Key '\(key)' not found:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } catch let DecodingError.valueNotFound(value, context) {
+                    print("Value '\(value)' not found:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } catch let DecodingError.typeMismatch(type, context)  {
+                    print("Type '\(type)' mismatch:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } catch {
+                    print("error: ", error)
+                }
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
     }
-    */
-
 }
