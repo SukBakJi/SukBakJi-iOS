c4ee4c5f6ee882a5223f400063d44b26c9ddaecb
Jaewift <pjk971113@naver.com>
2024-09-09 14:18:46 +0900

    [feat] 채팅UI 작업중

---
+++ Sukbakji/Chatting/Message/RoomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RoomTableViewCell.swift
+//  Sukbakji
+//
+//  Created by jaegu park on 9/9/24.
+//
+
+import UIKit
+
+class RoomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
