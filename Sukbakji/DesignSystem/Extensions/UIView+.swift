f55fff8b10b8edf464903e10dba456b463858066
hyunm1n-o <hmoh0703@naver.com>
2025-03-04 14:27:45 +0900

    [fix] Merge signup into develop with conflict resolution

---
+++ Sukbakji/DesignSystem/Extensions/UIView+.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+.swift
+//  Sukbakji
+//
+//  Created by 오현민 on 2/18/25.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: [UIView]) {
+        for view in views {
+            self.addSubview(view)
+        }
+    }
+}
