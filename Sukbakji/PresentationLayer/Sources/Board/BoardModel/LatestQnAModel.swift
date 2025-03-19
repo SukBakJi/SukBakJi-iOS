3167dd83de7b22038d752a34a45392781bc0d828
ctokkm <2271488.hansung.ac.kr>
2025-03-19 19:13:39 +0900

    [feat] 자유 카테고리 숨김 및 최신 질문글 3개 불러오기 모델 생성

---
+++ Sukbakji/PresentationLayer/Sources/Board/BoardModel/LatestQnAModel.swift
@@ -0,0 +1,13 @@
+//
+//  LatestQnAModel.swift
+//  Sukbakji
+//
+//  Created by KKM on 3/19/25.
+//
+
+import Foundation
+
+struct LatestQnAModel: Decodable {
+    let menu: String
+    let title: String
+}
